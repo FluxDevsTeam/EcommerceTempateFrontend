d75581bba1aa657a4e87671c5a674b46fab8a36e
siyegs <iyegeresuccess@gmail.com>
2025-04-05 17:35:40 +0100

    omo

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
-// filepath: c:\Users\USER\Desktop\EcommerceTemplate\EcommerceTempateFrontend\postcss.config.cjs
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
